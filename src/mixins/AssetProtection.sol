c3713383785eb84a51c3d7caafb8c0be8d2489c6
Philippe Dumonet <philippe@dumo.net>
2023-09-29 16:29:44 +0200

    ✨ Add recovery & transfer out methods

--- src/mixins/AssetProtection.sol
+++ src/mixins/AssetProtection.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {MerkleProofLib} from "solady/utils/MerkleProofLib.sol";
 import {LimiterConfig} from "../limiter/LimiterConfigLib.sol";
 import {DecreaseLimiter} from "../limiter/DecreaseLimiterLib.sol";
 import {MainDSM} from "../dsm/MainDSM.sol";
-import {FungibleAsset, NATIVE} from "../utils/FungibleAssetLib.sol";
+import {FungibleAssetLib, FungibleAsset, NATIVE} from "../utils/FungibleAssetLib.sol";
 
 /// @author philogy <https://github.com/philogy>
 abstract contract AssetProtector is MainDSM {
@@ -17,13 +18,20 @@ abstract contract AssetProtector is MainDSM {
     uint256 internal constant _UNLOCKED = 1;
     uint256 internal constant _LOCKED = 2;
 
+    uint256 internal constant _ERC1967_IMPL_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+    uint256 internal constant _ERC1967_BEACON_SLOT = 0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50;
+
     mapping(FungibleAsset => DecreaseLimiter) private _outflowLimiters;
     mapping(FungibleAsset => uint256) private _pendingOutflows;
     uint256 private _balanceDiffLock = _UNLOCKED;
 
+    bytes32 public confirmedValidEffectsTreeRoot;
+
     error NotContract();
     error DangerouslyLowDelay();
     error ReenteringBalanceDiff();
+    error UseOfEmergencyCallForUpgradeableContract();
+    error InvalidConfirmation();
 
     constructor(LimiterConfig defaultConfig, uint40 startDelay) MainDSM(startDelay) {
         _DEFAULT_LIMITER_CONFIG = defaultConfig;
@@ -46,9 +54,45 @@ abstract contract AssetProtector is MainDSM {
         _;
     }
 
+    /**
+     * @dev Allow `settlementMaster` to make arbitrary "rescue" calls if no upgradeability is
+     * detected. Can be disabled by overriding this function and having it always revert.
+     */
+    function emergencyRescueCall(address target, uint256 value, bytes calldata data)
+        external
+        virtual
+        onlySettlementMaster
+    {
+        address proxyImplementation;
+        address proxyBeacon;
+        /// @solidity memory-safe-assembly
+        assembly {
+            proxyImplementation := sload(_ERC1967_IMPL_SLOT)
+            proxyBeacon := sload(_ERC1967_BEACON_SLOT)
+        }
+        if (proxyImplementation != address(0) || proxyBeacon != address(0)) {
+            revert UseOfEmergencyCallForUpgradeableContract();
+        }
+        (bool success, bytes memory retdata) = target.call{value: value}(data);
+        if (!success) {
+            /// @solidity memory-safe-assembly
+            assembly {
+                revert(add(retdata, 0x20), mload(retdata))
+            }
+        }
+    }
+
     function settleAsset(FungibleAsset asset, address to, uint256 amount) external onlyThis {
         asset.transfer(to, amount);
         _pendingOutflows[asset] -= amount;
+    }
+
+    function setConfirmedValidEffectsTreeRoot(bytes32 newSetConfirmedValidEffectsTreeRoot)
+        external
+        onlySettlementMaster
+    {
+        confirmedValidEffectsTreeRoot = newSetConfirmedValidEffectsTreeRoot;
+        // TODO: Add events
     }
 
     function _recordETHInflow() internal {
@@ -94,6 +138,14 @@ abstract contract AssetProtector is MainDSM {
         assert(overflow == 0);
     }
 
+    function _safeTransfer(address token, address to, uint256 amount) internal {
+        _attemptTransfer(FungibleAssetLib.fromTokenAddress(token), to, amount);
+    }
+
+    function _safeTransferETH(address payable to, uint256 amount) internal {
+        _attemptTransfer(NATIVE, to, amount);
+    }
+
     function _attemptTransfer(FungibleAsset asset, address to, uint256 amount) private {
         DecreaseLimiter limiter = _outflowLimiters[asset];
         uint256 prevPendingOutflows = _pendingOutflows[asset];
@@ -107,6 +159,19 @@ abstract contract AssetProtector is MainDSM {
             // Rate limiter exceeded, skip limiter update and delay settlement of entire transfer.
             _schedule(address(this), 0, abi.encodeCall(this.settleAsset, (asset, to, amount)));
             _pendingOutflows[asset] = prevPendingOutflows + amount;
+        }
+    }
+
+    function _checkConfirmation(bytes32 effectId, bytes calldata confirmationProof) internal view override {
+        bytes32[] calldata proof;
+        assembly {
+            proof.offset := confirmationProof.offset
+            proof.length := div(proof.length, 32)
+        }
+        // Intermediary tree node *could* be interpreted as valid effect, however BaseDSM has
+        // additional checks ensuring that a given effectId is valid.
+        if (!MerkleProofLib.verifyCalldata({proof: proof, root: confirmedValidEffectsTreeRoot, leaf: effectId})) {
+            revert InvalidConfirmation();
         }
     }
 
